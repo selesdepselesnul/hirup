a3a4536af5dbe6474e85a126f54bff4673f99ad7
Moch Deden <deden.lime@gmail.com>
2019-12-28 09:32:38 +0700

    Update script

--- hirup.js
+++ hirup.js
@@ -1,8 +1,29 @@
 const batteryLevel = require('battery-level');
+const player = require('play-sound')({player: "mplayer"});
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+function playAlarm() {
+    player.play(`${__dirname}/hirup.mp3`, function(err){
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
 async function main() {
-    const batteryLevelValue = await batteryLevel();
-    console.log(batteryLevelValue);
+    setInterval(async function() {
+        const batteryLevelValue = await batteryLevel();
+        const batteryLevelValueRound = round(batteryLevelValue, 1);
+
+        console.log(batteryLevelValueRound)
+        if(batteryLevelValueRound <= 0.5) {
+            playAlarm();
+        }
+    }, 60000);
 }
 
 main();
